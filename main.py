9b3f9c5f46f67b72b11ad238dda69f5dfaf8ae9e
Wills17 <157613997+Wills17@users.noreply.github.com>
2024-02-02 09:45:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#import necessary libraries
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+
+n = "\n"            #Jump to next line
+
+#import dataset dataset
+diabetes = pd.read_csv('diabetes.csv')
+print(diabetes)
+print(diabetes.columns)
+print("\033c")      #Clear screen
+
+print("Overview of dataset:\n", diabetes.head(), n )
+
+print("Description of dataset:\n", diabetes.describe(), n)
+
+print("The shape of dataset is:", diabetes.shape, n)
+
