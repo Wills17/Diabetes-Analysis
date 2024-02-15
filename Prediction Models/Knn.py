d93015dd6408b93a1330ed3b139bb4e917b32ff3
Williams Odunayo <157613997+Wills17@users.noreply.github.com>
2024-02-15 15:06:43 +0100

    Update Knn.py

--- Prediction Models/Knn.py
+++ Prediction Models/Knn.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
@@ -12,8 +11,12 @@ from sklearn.metrics import classification_report
 n = "\n"               #Jump to next line
 print("\033c")         #Clear screen
 
-#import dataset
-diabetes = pd.read_csv('Dataset/diabetes.csv')
+#import dataset and ensure dataset is in folder
+try: 
+    diabetes = pd.read_csv('Dataset/diabetes.csv')
+except FileNotFoundError:
+    print("Please ensure that the dataset is in the folder 'Dataset' and try again.") 
+    quit()
 
 X = diabetes.drop(['Outcome'], axis=1)  #Remove the outcome column from the dataset
 y = diabetes['Outcome']                 #Assign outcome column to variable y
