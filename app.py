eadc64d36df9d216fdc825484e09549df47ae139
Williams Odunayo <williamsodunayo70@gmail.com>
2025-10-24 00:21:58 +0100

    Update flask app to include host and port.

--- app.py
+++ app.py
@@ -65,5 +65,7 @@ def predict():
     except Exception as e:
         return render_template('result.html', result=f"Error: {e}", model="N/A", risk=False)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
